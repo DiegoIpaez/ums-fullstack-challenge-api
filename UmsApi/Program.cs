2adf07c7c209cee1e654c4975a9ddc7557e04fd1
DiegoIpaez <diegoipaez2000@gmail.com>
2026-01-26 00:44:17 -0300

    fix: it has been enabled for all services

--- UmsApi/Program.cs
+++ UmsApi/Program.cs
@@ -96,6 +96,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowAll",
+        policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+    );
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -103,6 +111,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Habilita CORS global
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
